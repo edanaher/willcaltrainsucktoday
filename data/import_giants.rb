73b6012c2d6d48fb037f10202ce32de1f9c4da56
Evan Danaher <edanaher@edanaher.net>
2016-04-19 21:35:04 -0400

    Handle new START DATE first line in giants CSV

--- data/import_giants.rb
+++ data/import_giants.rb
@@ -7,7 +7,7 @@ db = PG.connect(dbname: 'willcaltrainsucktoday')
 
 File.open(ARGV[0]) do |file|
   file.each_line do |line|
-    next if line.start_with? "START_DATE"
+    next if line.start_with?("START_DATE") || line.start_with?("START DATE")
     date, time, _, teams, where = line.split(",")
     month, day, year = date.split("/")
     hour, minute, ampm = time.match("([0-9]+):([0-9]+) ([AP])M").captures
