7b421c1cd366f29be5a8b5b4e77c793989128e74
Evan Danaher <github@edanaher.net>
2017-05-05 09:11:05 -0400

    data: add Gemfile/.lock/gemset for running on NixOs.

---
+++ data/gemset.nix
@@ -0,0 +1,10 @@
+{
+  pg = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "03xcgwjs6faxis81jxf2plnlalg55dhhafqv3kvjxfr8ic7plpw5";
+      type = "gem";
+    };
+    version = "0.20.0";
+  };
+}
